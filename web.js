b10b0f795938139b18d3b551b1fd209efc284627
Jessica Goldsmith <jgoldsmith@invisiblechildren.com>
2013-07-12 18:54:52 +0000

    Delete unnecessary code

--- web.js
+++ web.js
@@ -8,12 +8,6 @@ app.get('/', function(request, response) {
   response.send(output);
 });
 
-//buf = new Buffer(256);
-//len = buf.write('\u00bd + \u00bc = \u00be', 0);
-//console.log(len + " bytes: " + buf.toString('utf8', 0, len));
-var output = fs.readFileSync('index.html','utf8');
-console.log(output);
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
