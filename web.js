34adf6a96aea380c5190abf2259ad670800c8db9
Jessica Goldsmith <jgoldsmith@invisiblechildren.com>
2013-07-12 17:12:39 +0000

    Read index.html from disk

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
-var output = fs.readFileSync('/index.html', buf.toString());
+//buf = new Buffer(256);
+//len = buf.write('\u00bd + \u00bc = \u00be', 0);
+//console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+var output = fs.readFileSync('index.html','utf8');
 console.log(output);
 
 var port = process.env.PORT || 5000;
