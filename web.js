675ad3b784c5dddd57424c7df613f90d27af90fc
Jessica Goldsmith <jgoldsmith@invisiblechildren.com>
2013-07-12 18:37:00 +0000

    Change code

--- web.js
+++ web.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+var output = fs.readFileSync('index.html', 'utf8');
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('index.html');
 });
 
 //buf = new Buffer(256);
 //len = buf.write('\u00bd + \u00bc = \u00be', 0);
 //console.log(len + " bytes: " + buf.toString('utf8', 0, len));
 var output = fs.readFileSync('index.html','utf8');
-response.send(output);
+console.log(output);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
