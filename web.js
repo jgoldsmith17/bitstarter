ba2fc6757acc166096f08d5fcbde5ef0986960f5
Jessica Goldsmith <jgoldsmith@invisiblechildren.com>
2013-07-12 15:49:39 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,8 +1,13 @@
+var express = require('express');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
+
+var output = fs.readFileSync('/index.html', buf.toString());
+console.log(output);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
